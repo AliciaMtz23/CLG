4a7873da6768c102465810453c2c17bfadb368d4
AliciaMtz23 <rosaalicame24@gmail.com>
2026-06-10 16:09:02 -0600

    fix: match image imports to actual git filenames

--- src/imagenes/imagenes.js
+++ src/imagenes/imagenes.js
@@ -1,8 +1,8 @@
 import ilustrativa1 from "./ilustrativa1.jpg";
 import ilustrativa2 from "./ilustrativa2.jpg";
 import ilustrativa3 from "./ilustrativa3.jpg";
-import ilustrativa4 from "./ilustrativa4.jpg";
-import ilustrativa5 from "./ilustrativa5.jpg";
+import ilustrativa4 from "./ilustrativa4.jpeg";
+import ilustrativa5 from "./ilustrativa5.JPG";
 
 const imagenes = {
   ilustrativa1,
